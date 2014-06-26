2204d91618fb481018a7ba49b1302246e146f9f9
navpreetsingh <navpreet.singh.k@gmail.com>
2014-06-26 15:00:11 +0530

    mysql upload

---
+++ bse_stock_export_direct_to_mysql.rb
@@ -0,0 +1,52 @@
+require 'csv'
+require 'mysql'
+require 'open-uri'
+
+stock = Array.new
+db = Mysql.new 'localhost', 'root', 'waheguru13', 'indian_stocks'
+stock_id = 1707
+ss_id = 1
+CSV.foreach("bse_bhav_copy.csv", :headers => true) do |row| 
+	if row[3] == "Q" && ss_id > 1706 && ss_id < 2200
+		t = Time.now
+		stock = row
+		stock_split = stock[1].split(" ").first
+		db.prepare("INSERT INTO BSE_stocks (
+			stock_name, 
+			stock_full_name) 
+		VALUES (?, ?)").execute(
+			stock_split, 
+			stock[1])
+		file = open("http://www.bseindia.com/stockinfo/stockprc2_excel.aspx?scripcd=#{stock[0]}&FromDate=01/01/2009&ToDate=06/24/2014&OldDMY=D")
+		stock_data = file.read	
+		stock_data = CSV.parse(stock_data)
+		stock_data.delete([])
+		stock_data.delete_at(0)
+		stock_data = stock_data.reverse		
+		stock_data.each do |s|
+			db.prepare("INSERT INTO BSE_stocks_details (
+				BSE_stock_id,
+				date,
+				open,
+				high,
+				low,
+				close,
+				volume, 
+				no_of_trades,
+				total_turnover)
+			VALUES (? , ?, ?, ?, ?, ?, ?, ?, ?)").execute(
+				stock_id,
+				Date.parse(s[0]).to_s,
+				s[1],
+				s[2],
+				s[3],
+				s[4],
+				s[6],
+				s[7],
+				s[8])
+		end
+		stock_id += 1 		
+		puts "#{stock[1]}: #{Time.now - t}"
+	end
+	ss_id += 1
+end
---
+++ nse_stock_export_direct_to_mysql.rb
@@ -0,0 +1,56 @@
+require 'csv'
+require 'mysql'
+require 'open-uri'
+
+stock = Array.new
+stock_data = Array.new
+db = Mysql.new 'localhost', 'root', 'waheguru13', 'indian_stocks'
+stock_id = 1
+CSV.foreach("nse_bhav_copy.csv", :headers => true) do |row| 
+	if row[1] == "EQ" && stock_id < 1001
+		t = Time.now
+		stock = row[0]
+		db.prepare("INSERT INTO NSE_stocks (
+			stock_name)			
+		VALUES (?)").execute(
+			stock)
+		stock = stock.gsub("&", "%26") if stock.include?("&")
+		file = open("http://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/getHistoricalData.jsp?symbol=#{stock}&fromDate=01-Jan-2009&toDate=24-Jun-2014&datePeriod=unselected&hiddDwnld=true")
+		stock_data = file.read	
+		stock_data = CSV.parse(stock_data)
+		stock_data.delete([]) 
+		stock_data.delete_at(0)
+		stock_data = stock_data.reverse	
+		# puts "1. #{stock_data.first}"
+		# puts "2. #{stock_data.last}"
+		
+		stock_data.each do |s|
+			begin
+				db.prepare("INSERT INTO NSE_stocks_details (
+					NSE_stock_id,
+					date,
+					open,
+					high,
+					low,
+					close,
+					volume, 
+					turnover)
+				VALUES (? , ?, ?, ?, ?, ?, ?, ?)").execute(
+					stock_id,
+					Date.parse(s[0]).to_s,
+					s[1],
+					s[2],
+					s[3],
+					s[5],
+					s[6],
+					s[7])
+			rescue Exception => e
+				puts e.message
+				puts "Stock: #{stock_split}"
+				puts "Data: #{s}"
+			end
+		end 
+		stock_id += 1	
+		puts "#{stock}: #{Time.now - t}"	
+	end		
+end
