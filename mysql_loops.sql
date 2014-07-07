78d3c61f79b6f6ff48acc2840f99725efd5038ba
navpreetsingh <navpreet.singh.k@gmail.com>
2014-07-07 18:50:26 +0530

    sql cmds

--- mysql_loops.sql
+++ mysql_loops.sql
@@ -20,7 +20,7 @@ CREATE PROCEDURE BSE_volume()
     	DECLARE average INT DEFAULT 1;    	
     	SELECT COUNT(*) FROM  `BSE_stocks`;
     	SET c = COUNT(*);
-    	WHILE a < 3162 DO
+    	WHILE a < c DO
 	        SELECT avg(volume) FROM 
 				(SELECT VOLUME FROM `BSE_stocks_details`
 				WHERE BSE_stock_id = a
@@ -34,7 +34,60 @@ CREATE PROCEDURE BSE_volume()
    		END WHILE;
 	END//
 
+	#Loop for high vol. stocks
+	DROP PROCEDURE IF EXISTS ll;
+	DELIMITER //
+	CREATE PROCEDURE indian_stocks.ll()
+	   	BEGIN
+	    	DECLARE a INT Default 1 ;
+	    	DECLARE c INT DEFAULT 1;	    	
+	    	SELECT COUNT(*) FROM  `BSE_stocks`;
+	    	SET c = COUNT(*);
+	    	WHILE a < c DO
+		        	update BSE_stocks set lp = a + 1 where id = a;		        
+		        SET a=a+1;	         
+	   		END WHILE;
+		END//
+
 
 SHOW PROCEDURE STATUS;
 SHOW FUNCTION STATUS;
+
+SELECT * FROM `bse_stocks_details`
+INNER JOIN bse_stocks
+on bse_stocks.id = bse_stocks_details.bse_stock_id
+where bse_stocks_details.bse_stock_id in (384,365, 162, 343, 651, 2398, 2857, 1987, 554, 983) and
+bse_stocks_details.date = "2013-10-21"
+
+#going up for last 3 days
+SELECT * FROM `bse4p_trends` 
+where d3_t = 3 and
+date = "2013-10-18" 
+order by avg_high desc
+limit 10
+
+#t7 & t3 r +ive and current day is also positive
+SELECT * FROM `bse4p_trends` 
+inner join bse_stocks_details on
+bse_stocks_details.bse_stock_id = bse4p_trends.bse_stock_id
+where bse4p_trends.d3_t > 0 and
+bse4p_trends.d7_t > 0 and 
+bse4p_trends.date = "2013-10-18" and
+bse_stocks_details.date = "2013-10-18" and
+bse_stocks_details.bs_signal = 1
+order by bse4p_trends.avg_high desc
+limit 10
+
+#t7 & t3 r +ive and current day is negative
+SELECT * FROM `bse4p_trends` 
+inner join bse_stocks_details on
+bse_stocks_details.bse_stock_id = bse4p_trends.bse_stock_id
+where bse4p_trends.d3_t > 0 and
+bse4p_trends.d7_t > 0 and 
+bse4p_trends.date = "2013-10-18" and
+bse_stocks_details.date = "2013-10-18" and
+bse_stocks_details.bs_signal = -1
+order by bse4p_trends.avg_high desc
+limit 10
+
 
