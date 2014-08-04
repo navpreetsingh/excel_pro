096f4c2bac96c1d9146c4e9e78d7419b09b111a2
navpreetsingh <navpreet.singh.k@gmail.com>
2014-08-04 10:09:01 +0530

    new sql cmds

--- bse_stock_export_direct_to_mysql.rb
+++ bse_stock_export_direct_to_mysql.rb
@@ -4,28 +4,28 @@ require 'open-uri'
 
 stock = Array.new
 db = Mysql.new 'localhost', 'root', 'waheguru13', 'indian_stocks'
-stock_id = 1707
-ss_id = 1
+stock_id = 1
+#ss_id = 1
 CSV.foreach("bse_bhav_copy.csv", :headers => true) do |row| 
-	if row[3] == "Q" && ss_id > 1706 && ss_id < 2200
+	if row[3] == "Q" 
 		t = Time.now
 		stock = row
-		stock_split = stock[1].split(" ").first
-		db.prepare("INSERT INTO BSE_stocks (
-			stock_name, 
-			stock_full_name) 
-		VALUES (?, ?)").execute(
-			stock_split, 
-			stock[1])
-		file = open("http://www.bseindia.com/stockinfo/stockprc2_excel.aspx?scripcd=#{stock[0]}&FromDate=01/01/2009&ToDate=06/24/2014&OldDMY=D")
+		# stock_split = stock[1].split(" ").first
+		# db.prepare("INSERT INTO BSE_stocks (
+		# 	stock_name, 
+		# 	stock_full_name) 
+		# VALUES (?, ?)").execute(
+		# 	stock_split, 
+		# 	stock[1])
+		file = open("http://www.bseindia.com/stockinfo/stockprc2_excel.aspx?scripcd=#{stock[0]}&FromDate=06/25/2014&ToDate=07/12/2014&OldDMY=D")
 		stock_data = file.read	
 		stock_data = CSV.parse(stock_data)
 		stock_data.delete([])
 		stock_data.delete_at(0)
 		stock_data = stock_data.reverse		
 		stock_data.each do |s|
-			db.prepare("INSERT INTO BSE_stocks_details (
-				BSE_stock_id,
+			db.prepare("INSERT INTO bse_stocks_details (
+				bse_stock_id,
 				date,
 				open,
 				high,
@@ -48,5 +48,5 @@ CSV.foreach("bse_bhav_copy.csv", :headers => true) do |row|
 		stock_id += 1 		
 		puts "#{stock[1]}: #{Time.now - t}"
 	end
-	ss_id += 1
+	#ss_id += 1
 end
